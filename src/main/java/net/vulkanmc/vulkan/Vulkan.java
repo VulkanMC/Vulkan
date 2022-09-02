59c0e24099c4b27d1825644760461af6246666b2
TomJuri <mail@tomjuri.de>
2022-09-02 19:05:51 +0200

    add start log and print system information

--- src/main/java/net/vulkanmc/vulkan/Vulkan.java
+++ src/main/java/net/vulkanmc/vulkan/Vulkan.java
@@ -4,10 +4,17 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.GlobalEventHandler;
 import net.vulkanmc.vulkan.listener.ItemDropping;
 import net.vulkanmc.vulkan.listener.ItemPickup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Vulkan {
 
     private static Vulkan instance;
+    private final String name = "Vulkan";
+    private final String version = "1.0.0";
+    private final String releaseType = "Alpha";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Vulkan.class);
     private final MinecraftServer minecraftServer = MinecraftServer.init();
     private final GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
 
@@ -20,10 +27,21 @@ public class Vulkan {
     private void startVulkan() {
         ItemDropping.register(globalEventHandler);
         ItemPickup.register(globalEventHandler);
+        MinecraftServer.setBrandName(name);
+        LOGGER.info("Starting " + name + " v" + version + "-" + releaseType);
+        printSysInfo();
         minecraftServer.start("0.0.0.0", 25565);
     }
 
     public static Vulkan getInstance() {
         return instance;
+    }
+
+    public void printSysInfo() {
+        System.out.println("System information:");
+        System.out.println("Java: " + System.getProperty("java.vendor") + " " + System.getProperty("java.version"));
+        System.out.println("OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + " " + System.getProperty("os.arch"));
+        System.out.println("Available Cores: " + Runtime.getRuntime().availableProcessors());
+        System.out.println("Available Memory: " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "MB");
     }
 }
